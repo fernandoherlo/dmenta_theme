0432fe75f0f20dfc982f46e62e5066b723d0342e
Fernando Hernáez <fernando.herlo@gmail.com>
2021-02-04 17:23:30 +0100

    Sidebar

--- functions.php
+++ functions.php
@@ -132,10 +132,32 @@ if ( ! function_exists( 'dmenta_theme_setup' ) ) :
                 'script',
             )
         );
+
+        // Add theme support for selective refresh for widgets.
+        add_theme_support( 'customize-selective-refresh-widgets' );
     }
 endif;
 add_action( 'after_setup_theme', 'dmenta_theme_setup' );
 
+/**
+ * Register widget area.
+ *
+ * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
+ */
+function dmenta_theme_widgets_init() {
+    register_sidebar(
+        array(
+            'name'          => esc_html__( 'Sidebar', 'dmenta_theme' ),
+            'id'            => 'sidebar-1',
+            'description'   => esc_html__( 'Add widgets here.', 'dmenta_theme' ),
+            'before_widget' => '<section id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</section>',
+            'before_title'  => '<h2 class="widget-title">',
+            'after_title'   => '</h2>',
+        )
+    );
+}
+add_action( 'widgets_init', 'dmenta_theme_widgets_init' );
 
 /**
  * Enqueue scripts and styles.
