2d7f703ef641139a46901c27a93276e3de516dfa
Fernando Hernáez <fernando.herlo@gmail.com>
2021-02-04 15:26:41 +0100

    Bootstrap & Laravel Mix

--- functions.php
+++ functions.php
@@ -13,25 +13,16 @@ License URI:        http://opensource.org/licenses/MIT
 
 
 function mix_tutorial_enqueue_styles() {
-  
     $theme = wp_get_theme();
-
     wp_enqueue_style( $theme->get('TextDomain') . '-app' , get_template_directory_uri() . '/public/css/app.css' , array() , $theme->get('Version') , 'all' );
-
 }
-
 add_action( 'wp_enqueue_scripts', 'mix_tutorial_enqueue_styles' );
 
 
 function mix_tutorial_enqueue_scripts() {
-  
     $theme = wp_get_theme();
-
-    wp_enqueue_script( $theme->get('TextDomain') . '-jquery' , 'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js' , array() , '3.3.1' , true );
     wp_enqueue_script( $theme->get('TextDomain') . '-app' , get_template_directory_uri() . '/public/js/app.js' , array() , $theme->get('Version') , true );
-
 }
-
 add_action( 'wp_enqueue_scripts', 'mix_tutorial_enqueue_scripts' );
 
 ?>
