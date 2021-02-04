056b6062f9eda2ea4d16d4646284f10ff0cab8e1
Fernando Hernáez <fernando.herlo@gmail.com>
2021-02-04 15:53:03 +0100

    Clean files

--- functions.php
+++ functions.php
@@ -29,9 +29,6 @@ if ( ! function_exists( 'dmenta_theme_setup' ) ) :
          */
         load_theme_textdomain( 'dmenta_theme', get_template_directory() . '/languages' );
 
-        // Add default posts and comments RSS feed links to head.
-        add_theme_support( 'automatic-feed-links' );
-
         /*
          * Let WordPress manage the document title.
          * By adding theme support, we declare that this theme does not use a
@@ -70,71 +67,10 @@ if ( ! function_exists( 'dmenta_theme_setup' ) ) :
                 'script',
             )
         );
-
-        // Set up the WordPress core custom background feature.
-        add_theme_support(
-            'custom-background',
-            apply_filters(
-                'dmenta_theme_custom_background_args',
-                array(
-                    'default-color' => 'ffffff',
-                    'default-image' => '',
-                )
-            )
-        );
-
-        // Add theme support for selective refresh for widgets.
-        add_theme_support( 'customize-selective-refresh-widgets' );
-
-        /**
-         * Add support for core custom logo.
-         *
-         * @link https://codex.wordpress.org/Theme_Logo
-         */
-        add_theme_support(
-            'custom-logo',
-            array(
-                'height'      => 250,
-                'width'       => 250,
-                'flex-width'  => true,
-                'flex-height' => true,
-            )
-        );
     }
 endif;
 add_action( 'after_setup_theme', 'dmenta_theme_setup' );
 
-/**
- * Set the content width in pixels, based on the theme's design and stylesheet.
- *
- * Priority 0 to make it available to lower priority callbacks.
- *
- * @global int $content_width
- */
-function dmenta_theme_content_width() {
-    $GLOBALS['content_width'] = apply_filters( 'dmenta_theme_content_width', 640 );
-}
-add_action( 'after_setup_theme', 'dmenta_theme_content_width', 0 );
-
-/**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
- */
-function dmenta_theme_widgets_init() {
-    register_sidebar(
-        array(
-            'name'          => esc_html__( 'Sidebar', 'dmenta_theme' ),
-            'id'            => 'sidebar-1',
-            'description'   => esc_html__( 'Add widgets here.', 'dmenta_theme' ),
-            'before_widget' => '<section id="%1$s" class="widget %2$s">',
-            'after_widget'  => '</section>',
-            'before_title'  => '<h2 class="widget-title">',
-            'after_title'   => '</h2>',
-        )
-    );
-}
-add_action( 'widgets_init', 'dmenta_theme_widgets_init' );
 
 /**
  * Enqueue scripts and styles.
@@ -152,11 +88,6 @@ function dmenta_theme_scripts() {
 add_action( 'wp_enqueue_scripts', 'dmenta_theme_scripts' );
 
 /**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
-
-/**
  * Custom template tags for this theme.
  */
 require get_template_directory() . '/inc/template-tags.php';
@@ -165,16 +96,3 @@ require get_template_directory() . '/inc/template-tags.php';
  * Functions which enhance the theme by hooking into WordPress.
  */
 require get_template_directory() . '/inc/template-functions.php';
-
-/**
- * Customizer additions.
- */
-require get_template_directory() . '/inc/customizer.php';
-
-/**
- * Load Jetpack compatibility file.
- */
-if ( defined( 'JETPACK__VERSION' ) ) {
-    require get_template_directory() . '/inc/jetpack.php';
-}
-
