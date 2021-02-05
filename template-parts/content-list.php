719bc8ec820e8de4cda731433af3e40d5de1b522
Fernando Hernáez <fernando.herlo@gmail.com>
2021-02-05 11:07:36 +0100

    Category in more

--- template-parts/content-list.php
+++ template-parts/content-list.php
@@ -32,11 +32,10 @@
                 ?>
                 See more 
                 <?php
-                    $categories_list = get_the_category_list( esc_html__( ', ', 'dmenta_theme' ) );
-                    if ( $categories_list ) {
-                        var_dump($categories_list);
-                        /* translators: 1: list of categories. */
-                        printf( '<span class="cat-links">' . esc_html__( '%1$s', 'dmenta_theme' ) . '</span>', $categories_list ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+                    $categories = get_the_category();
+ 
+                    if ( ! empty( $categories ) ) {
+                        echo esc_html( $categories[0]->name );   
                     }
                 ?>
             </div>
