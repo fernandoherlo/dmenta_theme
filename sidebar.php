05f5858aff945aace1f88006857958ddd3d16fbf
Fernando Hernáez <fernando.herlo@gmail.com>
2021-02-05 19:10:23 +0100

    Social icons

--- sidebar.php
+++ sidebar.php
@@ -15,7 +15,7 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 <aside>
     <div class="container">
         <div class="row">
-            <div class="col-12">
+            <div class="col-4">
                 <?php dynamic_sidebar( 'sidebar-1' ); ?>
             </div>
         </div>
