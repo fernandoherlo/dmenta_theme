6cf27d82eb9513701ba6fa22b0a02ad8c4f75fe6
Fernando Hernáez <fernando.herlo@gmail.com>
2021-02-05 11:10:48 +0100

    Category in more

--- template-parts/content-list.php
+++ template-parts/content-list.php
@@ -30,12 +30,11 @@
                 <?php
                 dmenta_theme_posted_on();
                 ?>
-                
                 <?php
                     $categories = get_the_category();
  
                     if ( ! empty( $categories ) ) {
-                        echo esc_html__('More about') . esc_html( strtolower($categories[0]->name) );   
+                        echo esc_html__('More about') . ' ' . esc_html( strtolower($categories[0]->name) );   
                     }
                 ?>
             </div>
