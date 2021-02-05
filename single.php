70beb5c8dd5f04cb7ad06f157a1016c06c4ced69
Fernando Hernáez <fernando.herlo@gmail.com>
2021-02-05 19:34:23 +0100

    Singlr

--- single.php
+++ single.php
@@ -20,17 +20,10 @@ get_header();
 
                     get_template_part( 'template-parts/content', get_post_type() );
 
-                    the_post_navigation(
-                        array(
-                            'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'dmenta_theme' ) . '</span> <span class="nav-title">%title</span>',
-                            'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'dmenta_theme' ) . '</span> <span class="nav-title">%title</span>',
-                        )
-                    );
-
                     // If comments are open or we have at least one comment, load up the comment template.
-                    if ( comments_open() || get_comments_number() ) :
-                        comments_template();
-                    endif;
+                    // if ( comments_open() || get_comments_number() ) :
+                    //     comments_template();
+                    // endif;
 
                 endwhile; // End of the loop.
                 ?>
