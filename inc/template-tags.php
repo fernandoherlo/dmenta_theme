6cbeb2e942505d9300053b92d5fe985986cb11fc
Fernando Hernáez <fernando.herlo@gmail.com>
2021-02-05 10:19:51 +0100

    Date post list

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -27,7 +27,7 @@ if ( ! function_exists( 'dmenta_theme_posted_on' ) ) :
 
 		$posted_on = sprintf(
 			/* translators: %s: post date. */
-			esc_html_x( 'Posted on %s', 'post date', 'dmenta_theme' ),
+			esc_html_x( '%s', 'post date', 'dmenta_theme' ),
 			'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
 		);
 
