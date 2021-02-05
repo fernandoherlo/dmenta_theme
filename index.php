4a4a828875db2a22327ee900cc0597b682bac3bc
Fernando Hernáez <fernando.herlo@gmail.com>
2021-02-05 10:31:09 +0100

    Remove Test

--- index.php
+++ index.php
@@ -41,10 +41,6 @@ get_header();
                     while ( have_posts() ) :
                         the_post();
 
-                        $post_categories = wp_get_post_categories( the_ID() );
-
-                        var_dump($post_categories);
-
                         /*
                          * Include the Post-Type-specific template for the content.
                          * If you want to override this in a child theme, then include a file
