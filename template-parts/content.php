776cb4793146d26f851e78eb0ae9708b4cc33ad2
Fernando Hernáez <fernando.herlo@gmail.com>
2021-02-05 19:42:56 +0100

    Templates

--- template-parts/content.php
+++ template-parts/content.php
@@ -10,51 +10,47 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
-		if ( is_singular() ) :
-			the_title( '<h1><strong>', '</strong></h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
+    <header class="entry-header">
+        <?php
+        if ( is_singular() ) :
+            the_title( '<h1><strong>', '</strong></h1>' );
+        else :
+            the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+        endif;
 
-		if ( 'post' === get_post_type() ) :
-			?>
-			<div class="entry-meta">
-				<?php
-				dmenta_theme_posted_on();
-				?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
+        if ( 'post' === get_post_type() ) :
+            ?>
+            <div class="entry-meta">
+                <?php
+                dmenta_theme_posted_on();
+                ?>
+            </div>
+        <?php endif; ?>
+    </header>
 
-	<div class="entry-content">
-		<?php
-		the_content(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'dmenta_theme' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				wp_kses_post( get_the_title() )
-			)
-		);
+    <div class="entry-content">
+        <?php
+        the_content(
+            sprintf(
+                wp_kses(
+                    /* translators: %s: Name of current post. Only visible to screen readers */
+                    __( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'dmenta_theme' ),
+                    array(
+                        'span' => array(
+                            'class' => array(),
+                        ),
+                    )
+                ),
+                wp_kses_post( get_the_title() )
+            )
+        );
 
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'dmenta_theme' ),
-				'after'  => '</div>',
-			)
-		);
-		?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php dmenta_theme_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-<?php the_ID(); ?> -->
+        wp_link_pages(
+            array(
+                'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'dmenta_theme' ),
+                'after'  => '</div>',
+            )
+        );
+        ?>
+    </div>
+</article>
