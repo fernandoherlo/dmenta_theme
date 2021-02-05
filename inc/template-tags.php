f376e963333abbb80bc164b9a2117bbfbc574090
Fernando Hernáez <fernando.herlo@gmail.com>
2021-02-05 11:04:59 +0100

    CSS link

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -20,7 +20,7 @@ if ( ! function_exists( 'dmenta_theme_posted_on' ) ) :
 		$time_string = sprintf(
 			$time_string,
 			esc_attr( get_the_date( DATE_W3C ) ),
-			esc_html( get_the_date() ),
+			esc_html( get_the_date() ) //,
 			// esc_attr( get_the_modified_date( DATE_W3C ) ),
 			// esc_html( get_the_modified_date() )
 		);
