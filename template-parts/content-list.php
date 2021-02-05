3a18a3f16003b955ee208b375452b03f7bd24d8e
Fernando Hernáez <fernando.herlo@gmail.com>
2021-02-05 10:39:55 +0100

    Link

--- template-parts/content-list.php
+++ template-parts/content-list.php
@@ -17,7 +17,9 @@
         if ( 'status' === get_post_format() ) :
             the_title( '<h3 class="entry-title">', '</h3>' );
         elseif ( 'link' === get_post_format() ) :
-            the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink( get_post_meta(get_the_ID(), 'link', true) ) ) . '" rel="bookmark">', '</a></h3>' );
+        ?>
+            <h3 class="entry-title"><a href="<?php esc_url( get_permalink( get_post_meta(get_the_ID(), 'link', true) ) ); ?>" rel="bookmark"><?php get_the_title( get_post_meta(get_the_ID(), 'link', true) ); ?></a></h3>
+        <?php
         else:
             the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' );
         endif;
