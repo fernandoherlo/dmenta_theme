1a3848bba421bc866aa16c22fc309857ffd9ae6d
Fernando Hernáez <fernando.herlo@gmail.com>
2021-02-05 10:04:41 +0100

    Add content-list

---
+++ template-parts/content-list.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package dmenta_theme
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+    <header class="entry-header">
+        <?php
+        
+        the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' );
+
+        if ( 'post' === get_post_type() ) :
+            ?>
+            <div class="entry-meta">
+                <?php
+                dmenta_theme_posted_on();
+                dmenta_theme_posted_by();
+                ?>
+            </div>
+        <?php endif; ?>
+    </header>
+
+    <div class="entry-content">
+        <?php
+        the_excerpt();
+        ?>
+    </div>
+
+</article>
