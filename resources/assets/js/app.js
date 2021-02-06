07513f176e7e6f3fabc98492da420ed5452ca15f
Fernando Hernáez <fernando.herlo@gmail.com>
2021-02-06 09:41:09 +0100

    Link JS

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 document.querySelector('body').classList.remove('loaded');
 
                 // Go
-                setTimeout(() => { window.location.href = element.href }, 400);
+                setTimeout(() => { window.location.href = element.href }, 700);
 
                 return false;
             } else {
