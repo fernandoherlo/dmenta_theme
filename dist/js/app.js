cb964bf888b9b77dc1ed296b82bb8289768df0d8
Fernando Hernáez <fernando.herlo@gmail.com>
2021-02-06 08:13:13 +0100

    JS Link

--- dist/js/app.js
+++ dist/js/app.js
@@ -3002,7 +3002,14 @@ __webpack_require__.r(__webpack_exports__);
 
 document.addEventListener("DOMContentLoaded", function () {
   console.log('asd');
+  wrap('main a', 'link');
 });
+
+function wrap(selector, cssClass) {
+  org_html = document.querySelector(selector).innerHTML;
+  new_html = '<div id="wrap ' + cssClass + '">' + org_html + '</div>';
+  document.querySelector(selector).innerHTML = new_html;
+}
 
 /***/ }),
 
--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -2,4 +2,13 @@ import 'bootstrap';
 
 document.addEventListener("DOMContentLoaded", function() {
     console.log('asd');
+
+    wrap('main a', 'link');
 });
+
+
+function wrap(selector, cssClass) {
+    org_html = document.querySelector(selector).innerHTML;
+    new_html = '<div id="wrap ' + cssClass + '">' + org_html + '</div>';
+    document.querySelector(selector).innerHTML = new_html;
+}
