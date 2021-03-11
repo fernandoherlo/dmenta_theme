a9ab2aecf8dfee402d990aa6daaf4061f01b640e
Fernando Hernáez <fernando.herlo@gmail.com>
2021-03-11 16:08:42 +0100

    Sidebar

--- sidebar.php
+++ sidebar.php
@@ -15,7 +15,7 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 <aside>
     <div class="container">
         <div class="row">
-            <div class="col-4">
+            <div class="col-12 col-sm-6 col-md-6 col-lg-4">
                 <?php dynamic_sidebar( 'sidebar-1' ); ?>
             </div>
             <div class="col-8 background"></div>
