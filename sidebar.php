f40c93cbcd887c9776b69204391235ffb3a0fad6
Fernando Hernáez <fernando.herlo@gmail.com>
2021-03-20 17:03:53 +0100

    Menu responsive

--- sidebar.php
+++ sidebar.php
@@ -15,10 +15,10 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 <aside>
     <div class="container">
         <div class="row">
-            <div class="col-12 col-sm-6 col-md-6 col-lg-4">
+            <div class="col-12 col-sm-6 col-md-6 col-lg-4 bgcol">
                 <?php dynamic_sidebar( 'sidebar-1' ); ?>
             </div>
-            <div class="col-12 col-xs-8 background"></div>
+            <div class="col-12 col-sm-8 background"></div>
         </div>
     </div>
 </aside>
