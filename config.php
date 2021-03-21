5a4fc1f207e69bc2be4b15669501f7c194f575d3
Fernando Hernáez <fernando.herlo@gmail.com>
2021-03-21 20:50:16 +0100

    Perloading

--- config.php
+++ config.php
@@ -3,5 +3,5 @@ if ( ! defined( 'DMENTA_POST_LINK' ) ) {
 	define('DMENTA_POST_LINK', false);
 }
 if ( ! defined( 'DMENTA_PRELOADING' ) ) {
-	define('DMENTA_PRELOADING', false);
+	define('DMENTA_PRELOADING', true);
 }
